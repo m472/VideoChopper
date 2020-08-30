50b3859a81ad827c9bba2f6ff70e0f1ee6f9030f
Mathias Graf <mathias.n.graf@gmail.com>
2020-08-30 17:51:04 +0200

    added saturation info to score, seems to work great :)

--- chopper.py
+++ chopper.py
@@ -7,52 +7,64 @@ import numpy as np
 def chop(filename, p1, p2):
     cap = cv.VideoCapture(filename)
 
-    buff = []
+    buff_gray = []
+    buff_saturation = []
     score_hist = []
     while cap.isOpened():
         ret, frame = cap.read()
 
         roi = np.array(frame[p1[1]:p2[1], p1[0]:p2[0], :])
 
+        roi_saturation = cv.cvtColor(roi, cv.COLOR_BGR2HSV)[:,:,1]
         roi_gray = cv.cvtColor(roi, cv.COLOR_BGR2GRAY)
 
-        if len(buff) == 0:
-            summe = np.zeros((roi_gray.shape), np.uint64)
+        if len(buff_gray) == 0:
+            summe_gray = np.zeros((roi_gray.shape), np.uint64)
+            summe_saturation = np.zeros((roi_gray.shape), np.uint64)
             num_pixels = frame.shape[0] * frame.shape[1]
 
-        summe += roi_gray
-        buff.append(roi_gray)
-        background = np.uint8(summe / float(len(buff)))
+        summe_gray += roi_gray
+        summe_saturation += roi_saturation
+        buff_gray.append(roi_gray)
+        buff_saturation.append(roi_saturation)
+        background = np.uint8(summe_gray / float(len(buff_gray)))
+        background_saturation = np.uint8(summe_gray / float(len(buff_saturation)))
 
-        if len(buff) > 10:
-            summe -= buff.pop(0)
+        if len(buff_gray) > 10:
+            summe_gray -= buff_gray.pop(0)
 
         diff = cv.absdiff(background, roi_gray)
-        score = cv.norm(background, roi_gray, cv.NORM_L1) / float(num_pixels)
+        saturation_diff = cv.absdiff(background_saturation, roi_saturation)
+        diff_product = np.uint8(255 * (diff / 255 * saturation_diff / 255))
+        score = cv.norm(diff_product, cv.NORM_L1) / float(num_pixels)
+        print(score)
 
         score_hist.append(score)
         if len(score_hist) > 50:
             score_hist.pop(0)
         avg_score = np.average(score_hist)
 
-        scaled = cv.resize(roi, (roi.shape[1]/10, roi.shape[0]/10))
+        scaled = cv.resize(roi, (int(roi.shape[1]/10), int(roi.shape[0]/10)),
                             interpolation=cv.INTER_NEAREST)
 
-        cv.rectangle(frame, p1, p2, (0, 255, 0) if avg_score > 1.1 else (0, 0, 255), 3)
+        cv.rectangle(frame, p1, p2, (0, 255, 0) if avg_score > 0.03 else (0, 0, 255), 3)
 
         Z = np.float32(scaled.reshape(-1, 3))
         criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 5, 1.0)
         K = 20
-        ret, label, center = cv.kmeans(Z, K, criteria, 5,
-                                        cv.KMEANS_RANDOM_CENTERS)
+        ret, label, center = cv.kmeans(Z, K, None, criteria, 5,
+                                       cv.KMEANS_RANDOM_CENTERS)
         palette = np.reshape(np.uint8(center), (5, 4, 3))
 
         cv.imshow('palette', cv.resize(palette, (500, 400),
                    interpolation=cv.INTER_NEAREST))
-        cv.imshow('original', frame)
+
+        cv.imshow('original', cv.resize(frame, (int(frame.shape[1]/2), int(frame.shape[0]/2))))
         cv.imshow('roi', roi)
         cv.imshow('background', background)
-        cv.imshow('diff', diff)
+        cv.imshow('diff_luminance', diff)
+        cv.imshow('diff_saturation', saturation_diff)
+        cv.imshow('diff_product', diff_product)
 
         if cv.waitKey(1) == 'q':
             break
